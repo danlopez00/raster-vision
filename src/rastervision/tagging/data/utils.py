59f027c7f7c06e90977ea84b4fa91aef1250f92f
Lewis Fishgold <lewfish@gmail.com>
2017-05-11 17:27:19 -0400

    Add Planet Kaggle data generator
    Also add some basic scaffolding for tagging problems

---
+++ src/rastervision/tagging/data/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib as mpl
+# For headless environments
+mpl.use('Agg') # NOQA
+import matplotlib.pyplot as plt
+
+
+def plot_sample(file_path, batch_x, batch_y, generator):
+    pass
